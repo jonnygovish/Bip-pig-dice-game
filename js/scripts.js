b54464d2edd548780719b6d00d3b4094717c66cd
John <jonnygovish@gmail.com>
2017-08-25 19:03:57 +0300

    turnScore Display displaying the turn score value

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,20 @@ function Player(Name, turnScore, totalScore) {
 }
 
 Player.prototype.roll = function () {
-    var dieRanVal = (Math.floor(Math.random() * 6)+1)
-    console.log(dieRanVal)
+    var dieRanVal = (Math.floor(Math.random() * 6) + 1)
+//    console.log(dieRanVal)
     if (dieRanVal === 1) {
         this.turnScore = 0
         alert("Ooops!The other players turn")
     } else {
         this.turnScore = this.turnScore + dieRanVal
+//        console.log(this.turnScore)
     }
-   return dieRanVal 
+    return dieRanVal
 }
-Player.prototype.hold = function (){
-    this.turnScore = 0
+Player.prototype.hold = function () {
     this.totalScore += this.turnScore
+    this.turnScore = 0
 }
 
 function Clearfield() {
@@ -26,7 +27,7 @@ function Clearfield() {
     $("#player2").val("");
 }
 
-
+var player1, player2;
 
 //user Interface
 $(document).ready(function () {
@@ -45,22 +46,25 @@ $(document).ready(function () {
         var player1Name = $("#player1").val();
         var player2Name = $("#player2").val();
         //create instances of the object
-        var player1 = new Player(player1Name, 0, 0);
-        var player2 = new Player(player2Name, 0, 0);
-    //Display the name of the users
+         player1 = new Player(player1Name, 0, 0);
+         player2 = new Player(player2Name, 0, 0);
+        //Display the name of the users
 
         $(".Player1name").text(player1Name);
         $(".Player2name").text(player2Name);
-        
+
         Clearfield();
-        
-        $(".roll1").click(function (event) {
+
+       
+    });
+     $(".roll1").click(function (event) {
             event.preventDefault();
-            var player1Roll = player1.roll();
             
-        $(".turn-score").html(player1Roll.turnScore);
-        console.log('displayed')
+            var player1Roll = player1.roll();
+
+            $(".turn-score").text(player1.turnScore);
+            $(".total-score").text(player1.totalScore);
+            console.log(player1.turnScore)
 
         });
-    })
 });
